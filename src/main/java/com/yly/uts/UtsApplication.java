03e2577a10eeba6832ef6049a14082bf4fa35136
zhechu <ling-yu-wang@qq.com>
2023-11-13 21:43:38 +0800

    feature:初始化

---
+++ src/main/java/com/yly/uts/UtsApplication.java
@@ -0,0 +1,13 @@
+package com.yly.uts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yly/uts/UtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yly.uts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
